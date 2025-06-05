155530ea895767a03a3648ac95bf11a8c0f409b0
NidhiNavin <nidhinavin@NidhiNavins-Air.lan>
2025-06-05 15:35:35 +0530

    Update team page layout and include team names

---
+++ src/app/team/images/divyamanpal_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/714-1daed6568b99ce81.js","static/chunks/pages/index-58b2a0154bf56c1c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
